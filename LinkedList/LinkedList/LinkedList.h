20c471355a60b2ad41d4a2087e7bbb445a0d175e
JDKendall <3dgiant@gmail.com>
2021-04-18 21:42:31 -0700

    Refactored add and remove functionality and tested out new features in main.cpp. Need to add in insertion functionality next.

--- LinkedList/LinkedList/LinkedList.h
+++ LinkedList/LinkedList/LinkedList.h
@@ -1,75 +1,142 @@
 #pragma once
 
-// linked type one to the next
+// linked type one to the nextItem
+
+// TODO
+// add in inserting functionality
+// add in more tests in main.cpp
 
 template <typename T>
 class LinkedList {
  public:
   LinkedList() : head(nullptr){};
 
- protected:
   typedef struct LinkedItem {
-    struct LinkedItem* next;
-    T data;
-  } * linkedItemPtr;
+    LinkedItem* nextItem = nullptr;
+    T* data = nullptr;
+  } * LinkedItemPtr;
 
- public:
-  linkedItemPtr head;
+  // add new LinkedItem by data type T
+  bool add(T* newData) {
+    if (!newData) {
+      return false;
+    }
 
-  bool pushBack(T newItem) {
+    LinkedItemPtr newItemPtr = new LinkedItem;
+
+    newItemPtr->data = newData;
+    newItemPtr->nextItem = nullptr;
+
+    if (!head) {
+      head = newItemPtr;
+      return true;
+    }
+
+    LinkedItemPtr currentTrackedItem = head;
+    // keep traversing the list until the last linked item's nextItem variable pointer is nullptr.
+    while (currentTrackedItem->nextItem != nullptr) {
+      currentTrackedItem = currentTrackedItem->nextItem;
+    }
+
+    currentTrackedItem->nextItem = newItemPtr;
+    return true;
+  }
+  // add new LinkedItem by created ptr struct
+  bool add(LinkedItemPtr newItem) {
     if (!newItem) {
       return false;
     }
 
-    linkedItemPtr itemPtr = new LinkedItem;
-
-    itemPtr->data = newItem;
-    itemPtr->next = nullptr;
-
     if (!head) {
-      head = itemPtr;
+      head = newItem;
       return true;
     }
 
-    linkedItemPtr currentLinkedItem = head;
-    // keep travering the next item pointer final item's next is nullptr , then the end of list has
-    // been found
-    while (currentLinkedItem->next != nullptr) {
-      currentLinkedItem = currentLinkedItem->next;
+    LinkedItemPtr currentTrackedItem = head;
+    // keep traversing the list until the last linked item's nextItem variable pointer is nullptr.
+    while (currentTrackedItem->nextItem != nullptr) {
+      currentTrackedItem = currentTrackedItem->nextItem;
     }
 
-    currentLinkedItem->next = itemPtr;
+    currentTrackedItem->nextItem = newItem;
     return true;
   }
 
-  T* pop();
-};
-// removing front stack
-template <typename T>
-inline T* LinkedList<T>::pop() {
-  if (!head) {
-    return nullptr; // better error handling as linked list is empty
+  // remove first instance of matching data type T
+  bool remove(T* removeData) {
+    if (!head || !removeData) {
+      return false;
+    }
+
+    LinkedItemPtr prevTrackedItem = head;
+    if (head->data == removeData) {
+      head = head->nextItem;
+      // delete old head ptr of list and then point it to the
+      delete (prevTrackedItem);
+      return true;
+    }
+
+    // keeping tabs on two linked list ptrs, the last known none nullptr prevTrackedItem and the
+    // nextItem item in the list currentTrackedItem searching through till a data match is found and
+    // updating the linked list or returning false if a data match was never found
+    LinkedItemPtr currentTrackedItem = prevTrackedItem->nextItem;
+    while (currentTrackedItem) {
+      if (currentTrackedItem->data == removeData) {
+        prevTrackedItem->nextItem = currentTrackedItem->nextItem;
+        delete (currentTrackedItem);
+        return true;
+      } else {
+        prevTrackedItem = currentTrackedItem;
+        currentTrackedItem = currentTrackedItem->nextItem;
+      }
+    }
+    return false;
   }
 
-  linkedItemPtr currentLinkedItem = head;
+  // remove matching struct ptr of LinkedItem
+  bool remove(LinkedItemPtr removeItem) {
+    if (!head || !removeItem) {
+      return false;
+    }
 
-  T data = currentLinkedItem->data;
-  head = currentLinkedItem->next;
-  delete(currentLinkedItem);
-  return &data;
-}
+    if (head == removeItem) {
+      head = head->nextItem;
+      // delete old head ptr of list and then point it to the
+      delete (removeItem);
+      return true;
+    }
 
-//template <typename T>
-//inline LinkedList<T>::linkedItemPtr LinkedList<T>::popEnd() {
-//  if (!head) {
-//    return nullptr;
-//  }
-//
-//  linkedItemPtr currentLinkedItem = head;
-//  linkedItemPtr lastLinkedItem = head;
-//  while (currentLinkedItem->next != nullptr) {
-//    currentLinkedItem = currentLinkedItem->next;
-//  }
-//
-//  return linkedItemPtr();
-//}
+    LinkedItemPtr prevTrackedItem = head;
+    LinkedItemPtr currentTrackedItem = prevTrackedItem->nextItem;
+    while (currentTrackedItem) {
+      if (currentTrackedItem == removeItem) {
+        prevTrackedItem->nextItem = currentTrackedItem->nextItem;
+        delete (removeItem);
+        return true;
+      }
+      else {
+        prevTrackedItem = currentTrackedItem;
+        currentTrackedItem = currentTrackedItem->nextItem;
+      }
+    }
+  }
+
+  
+  // TODO maybe this could be tracked as things are added and removed, so a full traversal is not needed
+  int Count() {
+    if (!head) {
+      return 0;
+    }
+
+    LinkedItemPtr currentTrackedItem = head;
+    int count = 1;
+    while (currentTrackedItem->nextItem != nullptr) {
+      count++;
+      currentTrackedItem = currentTrackedItem->nextItem;
+    }
+    return count;
+  }
+
+ private:
+  LinkedItemPtr head;
+};
--- LinkedList/LinkedList/main.cpp
+++ LinkedList/LinkedList/main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include "LinkedList.h"
 int main() {
-
   float n1 = 1.0;
   float n2 = 2.0;
   float n3 = 3.0;
 
   LinkedList<float> linkedList;
 
-  linkedList.pushBack(n1);
-  linkedList.pushBack(n2);
-  linkedList.pushBack(n3);
+  float t1 = 1.f;
 
-  linkedList.pop();
+  LinkedList<float>::LinkedItemPtr linkedItem1 = new LinkedList<float>::LinkedItem;
+  float x = 1.0f;
+  linkedItem1->data = &x;
 
-  std::cout << "Hello World!\n";
+  linkedList.add(&t1);
+  linkedList.add(linkedItem1);
+
+  std::cout << linkedList.Count() << "\n"; // should print 2
+
+  linkedList.remove(&t1);
+  linkedList.remove(linkedItem1);
+
+  std::cout << linkedList.Count() << "\n"; // should print 0
 }
